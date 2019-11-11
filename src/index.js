a92ecfb09f4cc4ead58d2731015649428ebea06a
NikolaiDokken <nikolard@stud.ntnu.no>
2019-11-11 19:56:47 +0100

    divided into frontend and backend, and added error messages to register

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
-import "./styles/Navbar.css";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
