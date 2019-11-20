0f441f67c3ababa755f5479900c0c9fe11933995
NikolaiDokken <nikolard@stud.ntnu.no>
2019-11-20 17:36:47 +0100

    fixed test database setup

--- dao/create_tables.sql
+++ dao/create_tables.sql
@@ -1,37 +1,26 @@
 DROP TABLE IF EXISTS sak;
 DROP TABLE IF EXISTS kategori;
 
-SET SQL_MODE
-="NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone
-="+00:00";
-
 CREATE TABLE sak
 (
   sak_id int(11) NOT NULL,
   forfatter varchar(50) DEFAULT NULL,
   overskrift varchar(200) NOT NULL,
   innhold text NOT NULL,
-  tidspunkt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-  ON
-  UPDATE CURRENT_TIMESTAMP,
-  bilde text
-  NOT NULL,
-  kategori_id int
-  (11) NOT NULL,
-  viktighet int
-  (11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  tidspunkt datetime NOT NULL DEFAULT '2019-11-20 20:15:00',
+  bilde text NOT NULL,
+  kategori_id int(11) NOT NULL,
+  viktighet int(11) NOT NULL
+)
 
-  CREATE TABLE kategori
-  (
-    kategori_id int(11) NOT NULL,
-    kategori_navn varchar(30) NOT NULL
-  );
+CREATE TABLE kategori
+(
+  kategori_id int(11) NOT NULL,
+  kategori_navn varchar(30) NOT NULL
+);
 
-  ALTER TABLE sak ADD PRIMARY KEY(sak_id);
+ALTER TABLE sak ADD PRIMARY KEY(sak_id);
 
-  ALTER TABLE kategori ADD PRIMARY KEY(kategori_id);
+ALTER TABLE kategori ADD PRIMARY KEY(kategori_id);
 
-  ALTER TABLE sak ADD CONSTRAINT sak_ibfk_1 FOREIGN KEY(kategori_id) REFERENCES kategori(kategori_id);
+ALTER TABLE sak ADD CONSTRAINT sak_ibfk_1 FOREIGN KEY(kategori_id) REFERENCES kategori(kategori_id);
