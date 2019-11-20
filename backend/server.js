3556218fcc2c74bd29044d0194bdcbd7ea40227b
NikolaiDokken <nikolard@stud.ntnu.no>
2019-11-20 17:14:28 +0100

    Added package for JEST and dao folder

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 var app = express();
 var mysql = require("mysql");
 app.use(bodyParser.json()); // for å tolke JSON
+const NewsTest = require("../dao/news.js");
 
 var pool = mysql.createPool({
   connectionLimit: 2,
@@ -12,6 +13,8 @@ var pool = mysql.createPool({
   database: "nikolard",
   debug: false
 });
+
+let newsTest = new NewsTest(pool);
 
 require("./router.js")(app, pool);
 
