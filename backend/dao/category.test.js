3ed8fc0390519c30da07e5acad2e6c9928411532
Nikolai Dokken <nikolaidokken@live.no>
2019-11-22 23:38:56 +0100

    Added all dao tests

--- backend/dao/category.test.js
+++ backend/dao/category.test.js
@@ -1,30 +1,6 @@
-var mysql = require("mysql");
-
 const Category = require("./category.js");
-const runsqlfile = require("./runsqlfile.js");
-
-// GitLab CI Pool
-var pool = mysql.createPool({
-  connectionLimit: 2,
-  host: "mysql",
-  user: "root",
-  password: "password",
-  database: "testDB",
-  debug: false,
-  multipleStatements: true
-});
 
 let category = new Category(pool);
-
-beforeAll(done => {
-  runsqlfile("dao/create_tables.sql", pool, () => {
-    runsqlfile("dao/create_testdata.sql", pool, done);
-  });
-});
-
-afterAll(() => {
-  pool.end();
-});
 
 test("get all categories from db", done => {
   function callback(status, data) {
